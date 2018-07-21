9d5a3225c0498827cf54ff51e9ff70c5c690c0c3
Kevin Chi <kevinchi118@gmail.com>
2018-07-21 19:12:07 +0000

    automatically backed up by learn

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,7 @@
+class CreateShows < ActiveRecord::Migration
+    create_table :shows do |t|
+        t.string :name
+        t.string :network
+        t.string :day
+        t.integer :rating
+end 
