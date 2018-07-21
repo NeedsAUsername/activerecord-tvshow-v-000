bca50f0d17613140054ed6152c1c1fc37c120251
Kevin Chi <kevinchi118@gmail.com>
2018-07-21 19:28:43 +0000

    automatically backed up by learn

--- app/models/Show.rb
+++ app/models/Show.rb
@@ -7,4 +7,13 @@ class Show < ActiveRecord::Base
     def self.most_popular_show
         self.where("rating = #{self.highest_rating}")[0]
     end
+
+    def self.lowest_rating
+        self.minimum(:rating)
+    end
+
+    def least_popular_show
+        self.where("rating = #{self.lowest_rating}")[0]
+    end
+    
 end
